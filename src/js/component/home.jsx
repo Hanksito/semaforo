716257c0d0f3826b51fdd1a9acb1135b4c2901d6
Hanksito <barjabalseiro@gmail.com>
2022-07-06 19:45:02 +0000

    semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,21 @@
-import React from "react";
+import React, {useState}from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [selected,setSelected] = useState("")
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+		<div id="TraficTop"></div>
+		<div id="container">
+			<div  className={selected === 'red'? 'red light selected' : 'red light'}onClick={()=>setSelected("red")}></div>
+			<div  className={selected === 'yellow' ? 'yellow light selected' : 'yellow light'}onClick={()=>setSelected("yellow")}></div>
+			<div  className={selected === 'green'? 'green light selected' : 'green light'}onClick={()=>setSelected("green")}></div>
 		</div>
+	</div>
 	);
 };
 
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,14 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+const list = document.querySelectorAll(".light")
+function activelink(){
+list.forEach((item)=>{
+    item.classList.remove("selected")})
+    this.classList.add("selected")}
+    list.forEach((item) =>{
+        item.addEventListener("click",activelink)})
+
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
